b8cb2beaac007f4f22c5eab88664116f72f6bb19
ForAnyPlatFormPrograms <147910428+ForAnyPlatFormPrograms@users.noreply.github.com>
2023-10-17 08:58:23 +0530

    Update SizeWindow.java

--- SizeWindow.java
+++ SizeWindow.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 
 public class SizeWindow extends SubWindow{
@@ -7,14 +6,11 @@ public class SizeWindow extends SubWindow{
     JScrollPane scrollPane = new JScrollPane();
     JPanel panelScollpane = new JPanel();
     JPanel panelWriting = new JPanel();
-    JTextField textField = new JTextField("Hello World!!");
+    JTextField textField = new JTextField("Hello World");
     SizeWindow(){
         this.setTitle("size");
         this.setLayout(new FlowLayout());
-        //GridBagConstraints gridBagConstraints = new GridBagConstraints();
         panelWriting.setLayout(new GridLayout());
-        //gridBagConstraints.gridx = 0;
-        //gridBagConstraints.gridy = GridBagConstraints.RELATIVE;
         panelWriting.add(textField);
         panelScollpane.setLayout(new GridLayout());
         String sizes[]={"10","20","30","40","50","60","70","80","90","100"};
@@ -22,8 +18,6 @@ public class SizeWindow extends SubWindow{
         scrollPane.add(list);
         scrollPane.setViewportView(list);
         panelScollpane.add(scrollPane);
-       // this.add(panelScollpane,gridBagConstraints);
-        //this.add(panelWriting,gridBagConstraints);
         this.add(panelScollpane);
         this.add(panelWriting);
         this.setResizable(false);
